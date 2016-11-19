b70ee35ccc7d3988fe1358701b68ec0707126b97
Kevin Coleman <kevinjamescoleman.7@gmail.com>
2016-11-18 21:30:04 -0800

    switch to sendgrid config.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,15 +57,15 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "concentration_#{Rails.env}"
 
   config.action_mailer.perform_deliveries = true
-  ActionMailer::Base.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'concentrationgame.herokuapp.com',
-    :authentication => :plain,
+  config.action_mailer.smtp_settings = {
+      :address   => "smtp.sendgrid.net",
+      :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
+      :enable_starttls_auto => true, # detects and uses STARTTLS
+      :user_name => ENV["SENDGRID_USERNAME"],
+      :password  => ENV["SENDGRID_PASSWORD"], # SMTP password is any valid API key, when user_name is "apikey".
+      :authentication => 'login',
+      :domain => 'concentrationgame.herokuapp.com', # your domain to identify your server when connecting
   }
-  ActionMailer::Base.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'http://concentrationgame.herokuapp.com' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
