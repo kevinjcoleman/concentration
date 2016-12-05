bc10f02ec7b949888d111eb1d2375001a169da23
Kevin Coleman <kevinjamescoleman.7@gmail.com>
2016-12-04 16:37:10 -0800

    Small refactor to games/show method.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -33,8 +33,7 @@ class GamesController < ApplicationController
     if @game.pending?
       flash[:info] = "That game hasn't started yet!"
       redirect_to game_invite_path(@game)
-    end
-    unless current_player.in?(@game.players)
+    elsif !current_player.in?(@game.players)
       flash[:danger] = "You aren't a player for that game."
       redirect_to root_path
     end
